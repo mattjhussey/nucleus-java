24fbbc719b65587eff451c6c36648d8c7db22c40
Matt Hussey <matthew.hussey@googlemail.com>
2017-10-20 19:01:09 +0100

    Basic Gradle config with JUnit.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import hussey.matt.Main;
+
+public class MainTest {
+    @Test
+    public void aTest() {
+	Main.main(null);
+	assertTrue(true);
+    }
+}
