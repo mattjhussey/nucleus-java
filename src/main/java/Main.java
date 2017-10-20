f7ff657770e7bb50c4735c447d38f16f41628a6f
Matt Hussey <matthew.hussey@googlemail.com>
2017-10-20 20:04:26 +0100

    Add jacoco - Code Coverage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 package hussey.matt;
 
 public class Main {
+    public void uncalled() {
+	System.out.println("Never gets run!");
+    }
+
     public static void main(String[] args) {
 	System.out.println("Hello World!");
     }
