6afcb8b3e4b84ce59224d30cb945db6dff1f670d
Matt Hussey <matthew.hussey@googlemail.com>
2017-10-21 15:46:45 +0100

    Add log4j via slf4j.

--- src/main/java/hussey/matt/Main.java
+++ src/main/java/hussey/matt/Main.java
@@ -1,7 +1,24 @@
 package hussey.matt;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 public final class Main {
+  // Define a static logger variable so that it references the
+  // Logger instance named "MyApp".
+  static Logger logger = Logger.getLogger(Main.class);
+
+  /**
+   * Entry point for the application.
+   */
   public static void main(String[] args) {
+    // Set up a simple configuration that logs on the console.
+    BasicConfigurator.configure();
+
+    logger.info("Entering application.");
+
     System.out.println("Hello World!");
+
+    logger.info("Exiting application.");
   }
 }
